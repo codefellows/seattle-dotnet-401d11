166db8eef46f2a8da3dbc5c0655dcd1a6d1a99e0
amanda <ajohnson764@gmail.com>
2020-07-09 14:40:42 -0700

    add demo code

---
+++ Class04/demo/DemoClass04/DemoClass04/Classes/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DemoClass04.Classes
+{
+    class Person
+    {
+        // Class is a template
+        // Class contains all of the attributes and behaviors that the "thing/object" can do. 
+
+        //Properties
+        // "Auto Implemented Properties"
+        // properties are defining features about the class that will be applied to the object
+        public string HairColor { get; set; }
+        public int Height { get; set; }
+        public string Name { get; set; }
+        public string EyeColor { get; set; }
+        public bool HasDegree { get; set; }
+        public int Age { get; set; }
+
+
+        // if not constructor is defined, One is GIVEN to you. it's invisible. 
+        // The GIVEN constructor is an empty constructor.
+        // empty constructor
+        public Person()
+        {
+
+        }
+        public Person(string name, string hairColor, int height)
+        {
+            HairColor = hairColor;
+            Name = name;
+            Height = height;
+        }
+
+        // this is an instance method (static is not present)
+        // it's behavior will change with the object
+        public string Walk()
+        {
+            return $"{Name} is Walking";
+        }
+
+        // static lives on the class level
+        // it is class dependant, and not obj dependent
+        public static string Walking()
+        {
+            return "I am statically walking";
+        }
+    }
+}
---
+++ Class04/demo/DemoClass04/DemoClass04/Program.cs
@@ -0,0 +1,110 @@
+﻿using DemoClass04.Classes;
+using System;
+
+namespace DemoClass04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //PersonExample();
+            ReferenceExample();
+        }
+
+        static void PersonExample()
+        {
+            // instantiate a object from a class
+
+            // simialr to the way we create variable
+            // TYPE name = "new" TYPE *Invoice Constructor*()
+            // instantiated a person from the Person class
+            // potato is an object.
+            Person potato = new Person();
+
+            potato.Name = "Josie  Cat";
+            potato.Age = 9;
+            potato.EyeColor = "green";
+            potato.HairColor = "gray/white";
+
+            Console.WriteLine($"{potato.Name} is {potato.Age} years old. They have {potato.EyeColor} eyes and {potato.HairColor} color hair");
+
+            Person goober = new Person();
+
+            // Setting of name
+            goober.Name = "Belle Kitty";
+            goober.Age = 15;
+            goober.EyeColor = "blue";
+            goober.HairColor = "brown";
+
+            // This GETS The value of name in goober
+            Console.WriteLine($"{goober.Name} is {goober.Age} years old. They have {goober.EyeColor} eyes and {potato.HairColor} color hair");
+
+            Console.WriteLine($"{potato.Name} and {goober.Name} are best friends!");
+
+
+            Person duck = new Person("Amanda", "Brown", 63);
+            duck.Age = 55;
+            duck.EyeColor = "brown";
+            Console.WriteLine($"{duck.Name} is {duck.Age} years old. They have {duck.EyeColor} eyes and {duck.HairColor} color hair");
+
+            int[] arrays = new int[] { 1, 2, 3 };
+
+            // object initializer
+            Person person = new Person()
+            {
+                Name = "Harry Potter",
+                EyeColor = "Green",
+                Age = 45,
+                HairColor = "Black",
+                HasDegree = false,
+            };
+
+            goober.Walk();
+
+            Person.Walking();
+
+
+
+        }
+        
+        public static void ReferenceExample()
+        {
+            Person original = new Person()
+            {
+                Name = "I am the original",
+                Age = 10,
+                EyeColor = "purple"
+            };
+
+            Person copyCat = original;
+
+            Console.WriteLine($"Original Name: {original.Name}");
+            Console.WriteLine($"CopyCat Name: {copyCat.Name}");
+
+            original.Name = "Oops, i've changed!";
+
+            Console.WriteLine($"Original Name: {original.Name}");
+            Console.WriteLine($"CopyCat Name: {copyCat.Name}");
+
+            copyCat.Name = "JK. i'm the copycat!";
+
+            Console.WriteLine($"Original Name: {original.Name}");
+            Console.WriteLine($"CopyCat Name: {copyCat.Name}");
+
+            Person newPerson = new Person()
+            {
+                Name = "Candy Cane"
+            };
+
+            copyCat = newPerson;
+
+            Console.WriteLine($"Original Name: {original.Name}");
+            Console.WriteLine($"CopyCat Name: {copyCat.Name}");
+            Console.WriteLine($"NewPerson Name: {newPerson.Name}");
+
+
+
+        }
+    }
+}
