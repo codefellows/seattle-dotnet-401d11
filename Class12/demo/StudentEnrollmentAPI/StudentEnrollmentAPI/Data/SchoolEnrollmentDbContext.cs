ac23726ab929cba3b6783ea67e01022b6f60dbb0
amanda <ajohnson764@gmail.com>
2020-07-21 13:15:33 -0700

    Class demo

---
+++ Class12/demo/StudentEnrollmentAPI/StudentEnrollmentAPI/Data/SchoolEnrollmentDbContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using StudentEnrollmentAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentEnrollmentAPI.Data
+{
+    // Your DBContext will be "AsyncInnDbContext.cs"
+    public class SchoolEnrollmentDbContext : DbContext
+    {
+        public SchoolEnrollmentDbContext(DbContextOptions<SchoolEnrollmentDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData(
+                new Student
+                {
+                    Id = 1,
+                    FirstName = "Jack",
+                    LastName = "Shepard",
+                    Birthdate = new DateTime(1970, 3, 5)
+                },
+                new Student
+                {
+                    Id = 2,
+                    FirstName = "Kate",
+                    LastName = "Austin",
+                    Birthdate = new DateTime(1980, 11, 11)
+
+                }
+                );
+
+            modelBuilder.Entity<Course>().HasData(
+                new Course
+                {
+                    Id = 1,
+                    CourseCode = "seattle-dotnet-401d11",
+                    Price = 100m,
+                    Technology = Technology.dotNet
+                },
+                new Course
+                {
+                    Id = 2,
+                    CourseCode = "seattle-201d100",
+                    Price = 50m,
+                    Technology = Technology.javascript
+                }
+                );
+        }
+
+        // to create an intial migration
+        // Install-Package Microsoft.EntityFrameworkCore.Tools
+        // add-migration {migrationName}
+        // add-migration intial
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ Class12/demo/StudentEnrollmentAPI/StudentEnrollmentAPI/Startup.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StudentEnrollmentAPI.Data;
+
+namespace StudentEnrollmentAPI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        // the appsettings.json, by default, is our "Configurations" for the app. 
+        // Set ourselves up for Dependency Injection
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // this is where all of our dependencies are going to live. 
+            // Enable the use of using controllers within the MVC convention
+            services.AddControllers();
+
+            // Register with the app, that the database exists, and what options to use for it. 
+            services.AddDbContext<SchoolEnrollmentDbContext>(options =>
+           {
+               // Install-Package Microsoft.EntityFrameworkCore.SqlServer
+               // Connection String = location where something lives. In our case, it's where our DB lives. 
+               // connection string contains the location, username, pw of your sql server...with our sql database directly.
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+           });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // Set our default routing for our request within the API application
+                // By default, our convention is {site}/[controller]/[action]/[id]
+                // id is not required, allowing it to be nullable
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
