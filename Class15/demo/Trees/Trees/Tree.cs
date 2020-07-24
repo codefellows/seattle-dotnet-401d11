acb19bde046b8fe0ab4d49e8360a7850dd7d06ff
amanda <ajohnson764@gmail.com>
2020-07-24 12:07:29 -0700

    trees implementation

---
+++ Class15/demo/Trees/Trees/Tree.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees
+{
+    public class Tree<T>
+    {
+        public Node<T> Root { get; set; }
+
+        public Tree()
+        {
+
+        }
+
+        public Tree(T value)
+        {
+            Node<T> root = new Node<T>(value);
+            Root = root;
+        }
+
+        public List<T> PreOrder(Node<T> root)
+        {
+            // conduct a preorder traversal
+            // capture each of the values in a LIST 
+            // return that list
+
+            List<T> traversal = new List<T>();
+            PreOrder(traversal, root);
+            return traversal;
+        }
+
+        private void PreOrder(List<T> traversal, Node<T> root)
+        {
+            traversal.Add(root.Value);
+
+            if (root.LeftChild != null)
+            {
+                PreOrder(traversal, root.LeftChild);
+            }
+
+            if (root.RightChild != null)
+            {
+                PreOrder(traversal, root.RightChild);
+            }
+
+        }
+
+    }
+}
